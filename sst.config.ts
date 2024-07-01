9d0096354b8bbfa50fb2f0564ef7f64968deb533
Kunal Gala <kunal.gala16@gmail.com>
2024-07-01 17:23:28 -0400

    fix: swapped out api token

--- sst.config.ts
+++ sst.config.ts
@@ -6,19 +6,16 @@ export default $config({
       name: "highland-skate-shop",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
-      // providers: { cloudflare: true },
+      providers: { cloudflare: true },
     };
   },
   async run() {
-    new sst.aws.Nextjs(
-      "MyWeb"
-      // {
-      // domain: {
-      //   name: "highlandskateshop.com",
-      //   redirects: ["www.highlandskateshop.com"],
-      //   dns: sst.cloudflare.dns({ zone: "787632f74d967d483802fed13e84e181" }),
-      // },
-      // }
-    );
+    new sst.aws.Nextjs("HighlandSkateShop", {
+      domain: {
+        name: "highlandskateshop.com",
+        redirects: ["www.highlandskateshop.com"],
+        dns: sst.cloudflare.dns({ zone: "787632f74d967d483802fed13e84e181" }),
+      },
+    });
   },
 });
